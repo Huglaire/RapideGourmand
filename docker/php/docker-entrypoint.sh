fbfa182b7f6c2ff91190872ab0bfa6528a237e58
Huglaire <pollon.hugo@gmail.com>
2026-07-22 18:15:07 +0200

    Recharge le cache Symfony après configuration JWT

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -36,13 +36,17 @@ EOF
 
 echo "Configuration JWT terminée."
 
+# Nettoyage du cache Symfony pour prendre en compte les nouvelles variables
+echo "Vidage du cache Symfony..."
+rm -rf var/cache/prod
+
 # Préparation des dossiers Symfony
 mkdir -p var/cache var/log var/sessions
 
 chown -R www-data:www-data var || true
 chmod -R 775 var || true
 
-# Si Railway fournit un port, on l'injecte dans la config nginx
+# Si Railway fournit un port, on l'injecte dans la configuration nginx
 if [ -n "$PORT" ]; then
     echo "Utilisation du port Railway : $PORT"
     sed -i "s/listen 80 default_server;/listen ${PORT} default_server;/g" /etc/nginx/conf.d/default.conf
