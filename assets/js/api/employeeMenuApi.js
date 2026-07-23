cff5a1db40f4f3e660d71dcb053b395f38966a13
Huglaire <pollon.hugo@gmail.com>
2026-07-23 08:43:02 +0200

    Ajout de la création des menus dans l'API employé

--- assets/js/api/employeeMenuApi.js
+++ assets/js/api/employeeMenuApi.js
@@ -1,16 +1,39 @@
 import { apiFetch } from './client.js';
 
 /**
- * Récupère tous les menus.
+ * Récupère tous les menus employés.
  */
 export async function getMenus() {
 
     const response = await apiFetch(
         '/api/menus/employee'
     );
-    return response.json();
+
+    return await response.json();
 
 }
+
+
+/**
+ * Crée un menu.
+ */
+export async function createMenu(data) {
+
+    const response = await apiFetch(
+        '/api/menus',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }
+    );
+
+    return await response.json();
+
+}
+
 
 /**
  * Désactive un menu.
@@ -25,6 +48,7 @@ export async function deleteMenu(id) {
     );
 
 }
+
 
 /**
  * Réactive un menu.
